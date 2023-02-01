f59c52c861c540c358adb7800f25f4ecd9c2f03d
samandar-mee <samandarcoderdev@gmail.com>
2023-02-01 14:54:08 +0500

    Detail Screen Completed

--- app/src/main/java/com/sdk/foddy/ui/detail/ing/IngredientsScreen.kt
+++ app/src/main/java/com/sdk/foddy/ui/detail/ing/IngredientsScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -22,7 +21,9 @@ fun IngredientsScreen(
     LazyColumn(
         contentPadding = PaddingValues(5.dp), modifier = Modifier
             .fillMaxSize()
-            .background(color = MaterialTheme.colorScheme.primary)
+            .background(
+                DescColor
+            )
     ) {
         ingredients?.let {
             items(it) { item ->
